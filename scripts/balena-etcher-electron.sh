9a41ea5808c4e2477c0dcef0c01cc2556a538940
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2022-03-12 13:50:51 -0500

    add balena-etcher-electron package

---
+++ scripts/balena-etcher-electron.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Core functions
+source api
+TOKENSCRIPT="/root/token.sh"
+if [ ! -f $TOKENSCRIPT ]; then
+    error "$TOKENSCRIPT couldn't be found. Exiting."
+fi
+source $TOKENSCRIPT
+
+status "Updating balena-etcher-electron."
+BALENAETCHERELECTRON_API=`curl -s --header "Authorization: token $token" https://api.github.com/repos/Itai-Nelken/Etcher-arm-32-64/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+BALENAETCHERELECTORN_DATAFILE="$HOME/dlfiles-data/balena-etcher-electron.txt"
+if [ ! -f "$BALENAETCHERELECTORN_DATAFILE" ]; then
+    status "$BALENAETCHERELECTORN_DATAFILE does not exist."
+    status "Grabbing the latest release from GitHub."
+    echo $BALENAETCHERELECTRON_API > $BALENAETCHERELECTORN_DATAFILE
+fi
+BALENAETCHERELECTRON_CURRENT="$(cat ${BALENAETCHERELECTORN_DATAFILE})"
+if [ "$BALENAETCHERELECTRON_CURRENT" != "$BALENAETCHERELECTRON_API" ]; then
+    status "balena-etcher-electron isn't up to date. updating now..."
+    curl -s --header "Authorization: token $token" https://api.github.com/repos/Itai-Nelken/Etcher-arm-32-64/releases/latest \
+      | grep browser_download_url \
+      | grep 'arm64.deb"' \
+      | cut -d '"' -f 4 \
+      | xargs -n 1 curl -L -o balena-etcher-electron_${BALENAETCHERELECTRON_API}_arm64.deb || error "Failed to download balena-etcher-electron:arm64!"
+    curl -s --header "Authorization: token $token" https://api.github.com/repos/Itai-Nelken/Etcher-arm-32-64/releases/latest \
+      | grep browser_download_url \
+      | grep 'armv7l.deb"' \
+      | cut -d '"' -f 4 \
+      | xargs -n 1 curl -L -o balena-etcher-electron_${BALENAETCHERELECTRON_API}_armhf.deb || error "Failed to download balena-etcher-electron:armhf!"
+
+    mv balena-etcher-electron* $PKGDIR
+    echo $BALENAETCHERELECTRON_API > $BALENAETCHERELECTORN_DATAFILE
+    green "balena-etcher-electron downloaded successfully."
+fi
+green "balena-etcher-electron is up to date."
